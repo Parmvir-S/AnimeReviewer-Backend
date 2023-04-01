c6e96473ee4e10e776325f509681008d086330e8
Parmvir-S <parmvir.shergill@gmail.com>
2023-03-31 22:43:22 -0700

    Added error handling for db connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,4 +2,6 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
-});
+}).catch((error) => {
+  console.log("DB Connection Failed", error);
+})
